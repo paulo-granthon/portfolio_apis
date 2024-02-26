ebc4ec57380cca9b9b0eaefff7e5d5f779a2d491
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-25 21:15:32 -0300

    feat: handle errors in `NewAPIServer` and `Start`

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,20 +12,35 @@ type APIServer struct {
 	listenAddress string
 }
 
-func NewAPIServer(port int) *APIServer {
+func NewAPIServer(port int) (*APIServer, error) {
+	if port < 1 || port > 65535 {
+		return nil, fmt.Errorf("Invalid listen address: %v", port)
+	}
+
 	return &APIServer{
 		listenAddress: fmt.Sprintf(":%v", port),
-	}
+	}, nil
 }
 
-func (s *APIServer) Start() error {
+func (s *APIServer) Start() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", RootEndpoint().Create()).Methods("GET")
 
 	log.Println("Starting server on", s.listenAddress)
 
-	return http.ListenAndServe(s.listenAddress, router)
+	error := http.ListenAndServe(s.listenAddress, router)
+	if error != nil {
+		log.Fatal("Error starting server:", error)
+	}
+}
+
 func main() {
-	server := NewAPIServer(3333)
+	server, error := NewAPIServer(3333)
+
+	if error != nil {
+		log.Fatal("Error creating server:", error)
+		return
+	}
+
 	server.Start()
 }
