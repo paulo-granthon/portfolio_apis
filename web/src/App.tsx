0542c5d10b54881cdc8083a539cfcf69519b527c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-27 23:49:10 -0300

    chore(front): add example `Project` to `App`

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,19 @@
+import Project from "./components/project";
+import { ProjectSchema } from "./schemas/project";
 
-function App() {
-  return <h1>Porfolio APIs</h1>;
+export default function App() {
+  const project: ProjectSchema = {
+    id: 1,
+    name: "Project 1",
+    semester: 1,
+    summary: "This is a summary",
+    company: "Company 1",
+    url: "https://example.com",
+  };
+  return (
+    <>
+      <h1>Porfolio APIs</h1>
+      <Project {...project} />
+    </>
+  );
 }
-
-export default App;
