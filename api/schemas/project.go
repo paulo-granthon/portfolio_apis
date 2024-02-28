366add7cd79b7ea475a50a05650877d9f4a2020f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-28 01:22:07 -0300

    feat(back): create `GetProjectRequest` & `UpdateProjectRequest` schemas

--- api/schemas/project.go
+++ api/schemas/project.go
@@ -2,10 +2,19 @@ package schemas
 
 import "models"
 
+type GetProjectRequest struct {
+	Id uint64 `json:"id"`
+}
+
 type CreateProjectRequest struct {
 	Project models.CreateProject `json:"project"`
 }
 
 type CreateProjectResponse struct {
 	Id uint64 `json:"id"`
+}
+
+type UpdateProjectRequest struct {
+	Id      uint64         `json:"id"`
+	Project models.Project `json:"project"`
 }
