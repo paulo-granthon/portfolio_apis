7307374571e5cd57a40cbe7ba72e89874de823b2
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-28 01:42:30 -0300

    feat(front): create remaining `services` functions

--- web/src/services/project.ts
+++ web/src/services/project.ts
@@ -1,7 +1,50 @@
 import { ProjectSchema } from "../schemas/project";
 
-export async function getProjects() {
-  return fetch(import.meta.env.VITE_API_URL + "/projects")
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function getProjects(): Promise<ProjectSchema[]> {
+  return fetch(API_URL + "/projects")
     .then((response) => response.json())
-    .then((data) => data as ProjectSchema[]);
+    .then((data) => data);
+}
+
+export async function getProject(id: string): Promise<ProjectSchema> {
+  return fetch(API_URL + "/projects/" + id)
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
+export async function createProject(
+  project: ProjectSchema,
+): Promise<ProjectSchema> {
+  return fetch(API_URL + "/projects", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(project),
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
+export async function updateProject(
+  id: string,
+  project: ProjectSchema,
+): Promise<ProjectSchema> {
+  return fetch(API_URL + "/projects/" + id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(project),
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
+export async function deleteProject(id: string): Promise<void> {
+  return fetch(API_URL + "/projects/" + id, {
+    method: "DELETE",
+  }).then((response) => response.json());
 }
