d6a42acb436808a80c798beba00e3fc3d523028d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-28 19:39:23 -0300

    feat: example `stylex` styles in `App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,13 @@
 import Project from "./components/project";
 import { ProjectSchema } from "./schemas/project";
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  base: {
+    backgroundColor: "gray",
+    fontSize: 40,
+  },
+});
 
 export default function App() {
   const project: ProjectSchema = {
@@ -11,9 +19,9 @@ export default function App() {
     url: "https://example.com",
   };
   return (
-    <>
+    <div {...stylex.props(styles.base)}>
       <h1>Porfolio APIs</h1>
       <Project {...project} />
-    </>
+    </div>
   );
 }
