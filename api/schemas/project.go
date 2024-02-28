5807ab2cbee37d6f01d8ef5b51da50ad7dd32d77
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-28 01:21:24 -0300

    chore(back): use `Project` instead of bare fields in `CreateProjectRequest`

--- api/schemas/project.go
+++ api/schemas/project.go
@@ -1,11 +1,9 @@
 package schemas
 
+import "models"
+
 type CreateProjectRequest struct {
-	Name     string `json:"name"`
-	Semester uint8  `json:"semester"`
-	Company  string `json:"company"`
-	Summary  string `json:"summary"`
-	Url      string `json:"url"`
+	Project models.CreateProject `json:"project"`
 }
 
 type CreateProjectResponse struct {
