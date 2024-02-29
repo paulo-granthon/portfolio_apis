84b7b5784de855b6258a7e9e9d30620f2bae73a6
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-28 22:23:47 -0300

    feat(back): create `User` & `YearSemester` types

---
+++ api/models/user.go
@@ -0,0 +1,43 @@
+package models
+
+type YearSemester struct {
+	Year     uint16 `json:"year"`
+	Semester bool   `json:"semester"`
+}
+
+func NewYearSemester(
+	year uint16,
+	semester bool,
+) YearSemester {
+	return YearSemester{
+		Year:     year,
+		Semester: semester,
+	}
+}
+
+type User struct {
+	Id                 uint64        `json:"id"`
+	Name               string        `json:"name"`
+	Summary            *string       `json:"summary"`
+	SemesterMatriculed *YearSemester `json:"semesterMatriculed"`
+	GithubUsername     *string       `json:"githubUsername"`
+	Password           string        `json:"password"`
+}
+
+func NewUser(
+	id uint64,
+	name string,
+	summary *string,
+	semesterMatriculed *YearSemester,
+	githubUsername *string,
+	password string,
+) User {
+	return User{
+		Id:                 id,
+		Name:               name,
+		Summary:            summary,
+		SemesterMatriculed: semesterMatriculed,
+		GithubUsername:     githubUsername,
+		Password:           password,
+	}
+}
