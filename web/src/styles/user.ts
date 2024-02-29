f2fd58f3efa880362d5e6a05149cb00a70ff6ae0
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-29 19:47:26 -0300

    feat(front): create `userStyles` file and `profilePicture` style class

--- web/src/styles/user.ts
+++ web/src/styles/user.ts
@@ -1,3 +1,11 @@
 import * as stylex from "@stylexjs/stylex";
 
-const styles = stylex.create({});
+const styles = stylex.create({
+  profilePicture: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+});
+
+export const profilePicture = stylex.props(styles.profilePicture);
