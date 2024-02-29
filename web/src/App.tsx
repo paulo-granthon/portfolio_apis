9a53efc9943e5abe11aa0ef4db026baaf3f73a03
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-28 22:13:47 -0300

    chore(front): use `Portfolio` in `App` and bring latest changes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,18 @@
-import Project from "./components/project";
-import { ProjectSchema } from "./schemas/project";
-import * as stylex from "@stylexjs/stylex";
-
-const styles = stylex.create({
-  base: {
-    backgroundColor: "gray",
-    fontSize: 40,
-  },
-});
+import Portfolio from "./pages/portfolio";
+import { UserSchema } from "./schemas/user";
 
 export default function App() {
-  const project: ProjectSchema = {
-    id: 1,
-    name: "Project 1",
-    semester: 1,
-    summary: "This is a summary",
-    company: "Company 1",
-    url: "https://example.com",
+  const user: UserSchema = {
+    id: 0,
+    name: "paulo",
+    summary:
+      "Backend developer intern at @gorilainvest | Database technologist student at FATEC | Self titled full-stack developer",
+    semesterMatriculated: {
+      year: 2022,
+      semester: 2,
+    },
+    githubUsername: "paulo-granthon",
   };
-  return (
-    <div {...stylex.props(styles.base)}>
-      <h1>Porfolio APIs</h1>
-      <Project {...project} />
-    </div>
-  );
+
+  return <Portfolio {...user} />;
 }
