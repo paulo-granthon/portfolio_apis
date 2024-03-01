8d00185289f6983cd209dc9bfbb68b464a0e5539
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-01 01:06:39 -0300

    fix: allow no `DB_NAME` in `pgconnect`

--- pgconnect.bash
+++ pgconnect.bash
@@ -3,26 +3,38 @@
 # function to imediately execute a query on connect
 function watch_query() {
     query=${1:-"show databases"}
-    PGPASSWORD="${DB_PASSWORD}" watch -etn 1 "psql -h${DB_HOST} -U${DB_USER} -d${DB_NAME} -c '${query}'"
+    DB_NAME_MAYBE=""
+    if [ -n "${DB_NAME}" ]; then
+        DB_NAME_MAYBE="-d ${DB_NAME}"
+    fi
+    PGPASSWORD="${DB_PASSWORD}" watch -etn 1 "psql -h${DB_HOST} -U${DB_USER} ${DB_NAME_MAYBE} -c '${query}'"
     clear
 }
 
 # Function to execute a query once
 function execute_query_once() {
     query=${1:-"show databases"}
-    PGPASSWORD="${DB_PASSWORD}" psql -h"${DB_HOST}" -U"${DB_USER}" -d"${DB_NAME}" -c "${query}"
+    DB_NAME_MAYBE=""
+    if [ -n "${DB_NAME}" ]; then
+        DB_NAME_MAYBE="-d ${DB_NAME}"
+    fi
+    PGPASSWORD="${DB_PASSWORD}" psql -h"${DB_HOST}" -U"${DB_USER}" "${DB_NAME_MAYBE}" -c "${query}"
 }
 
 function execute_sql_script_file() {
     script_file=${1}
-
     if [ ! -f "${script_file}" ]; then
         echo "File ${script_file} does not exist."
         echo -e "Usage: $0 <db_index> --script <script_file>"
         exit 1
     fi
 
-    PGPASSWORD="${DB_PASSWORD}" psql -h"${DB_HOST}" -U"${DB_USER}" -d"${DB_NAME}" -f "${script_file}"
+    DB_NAME_MAYBE=""
+    if [ -n "${DB_NAME}" ]; then
+        DB_NAME_MAYBE="-d ${DB_NAME}"
+    fi
+
+    PGPASSWORD="${DB_PASSWORD}" psql -h"${DB_HOST}" -U"${DB_USER}" "${DB_NAME_MAYBE}" -f "${script_file}"
 }
 
 # Load the environment variables
