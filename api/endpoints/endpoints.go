386731181230eb53ffc36408ef51837e5b1d66d8
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-01 01:16:08 -0300

    refactor(back): use `NewMethod` and `WriteJSON` in `RootEndpoints`

--- api/endpoints/endpoints.go
+++ api/endpoints/endpoints.go
@@ -16,13 +16,11 @@ func RootEndpoint() server.Endpoint {
 	return server.Endpoint{
 		Path: "/",
 		Methods: []server.Method{
-			{
-				Method: "GET",
-				Func: func(s server.Server, w http.ResponseWriter, r *http.Request) error {
-					w.Write([]byte("Hello, World!"))
-					return nil
-				},
-			},
+			server.NewMethod("GET", GetRoot),
 		},
 	}
+}
+
+func GetRoot(s server.Server, w http.ResponseWriter, r *http.Request) error {
+	return server.WriteJSON(w, http.StatusOK, "Hello, world!")
 }
