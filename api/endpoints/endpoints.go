a351a5050a253ad5c9577298587fc218d7daf393
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-01 01:20:29 -0300

    feat(back): create `/migrate` endpoint

--- api/endpoints/endpoints.go
+++ api/endpoints/endpoints.go
@@ -28,9 +28,23 @@ func RootEndpoints() []server.Endpoint {
 				server.NewMethod("GET", GetRoot),
 			},
 		},
+		{
+			Path: "/migrate",
+			Methods: []server.Method{
+				server.NewMethod("POST", Migrate),
+			},
+		},
 	}
 }
 
 func GetRoot(s server.Server, w http.ResponseWriter, r *http.Request) error {
 	return server.WriteJSON(w, http.StatusOK, "Hello, world!")
+}
+
+func Migrate(s server.Server, w http.ResponseWriter, r *http.Request) error {
+	err := s.Storage.Migrate()
+	if err != nil {
+		return server.SendError(w, err)
+	}
+	return server.WriteJSON(w, http.StatusOK, "Migrated")
 }
