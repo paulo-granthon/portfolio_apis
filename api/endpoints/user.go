1f1f62dc9e0a4a7d079147725aaa7fc3a688714e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-29 21:55:14 -0300

    feat(back): implement body for `UserEndpoint` functions

--- api/endpoints/user.go
+++ api/endpoints/user.go
@@ -1,8 +1,11 @@
 package endpoints
 
 import (
+	"models"
 	"net/http"
+	"schemas"
 	"server"
+	"strconv"
 )
 
 func UserEndpoints() []server.Endpoint {
@@ -11,6 +14,7 @@ func UserEndpoints() []server.Endpoint {
 			Path: "/users",
 			Methods: []server.Method{
 				server.NewMethod("GET", GetUsers),
+				server.NewMethod("POST", RegisterUser),
 				server.NewMethod("POST", CreateUser),
 			},
 		},
@@ -26,21 +30,148 @@ func UserEndpoints() []server.Endpoint {
 }
 
 func GetUsers(s server.Server, w http.ResponseWriter, r *http.Request) error {
-	return nil
+	userModule, err := s.Storage.GetUserModule()
+	if err != nil {
+		return err
+	}
+
+	users, err := userModule.Get()
+	if err != nil {
+		return server.SendError(w, err)
+	}
+	return server.WriteJSON(w, http.StatusOK, users)
 }
 
 func GetUser(s server.Server, w http.ResponseWriter, r *http.Request) error {
-	return nil
+	idStr, err := server.GetRequestParam(r, "id")
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	id, err := strconv.ParseUint(*idStr, 10, 64)
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	userModule, err := s.Storage.GetUserModule()
+	if err != nil {
+		return err
+	}
+
+	user, err := userModule.GetById(id)
+	if err != nil {
+		return server.WriteJSON(w, http.StatusNotFound, server.Error{Error: "user not found"})
+	}
+
+	return server.WriteJSON(w, http.StatusOK, user)
+}
+
+func RegisterUser(s server.Server, w http.ResponseWriter, r *http.Request) error {
+	var request schemas.RegisterUserRequest
+	if error := server.ReadJSON(r, &request.Payload); error != nil {
+		return server.SendError(w, error)
+	}
+
+	user := models.NewRegisterUser(
+		request.Payload.Name,
+		request.Payload.Password,
+	)
+
+	userModule, err := s.Storage.GetUserModule()
+	if err != nil {
+		return err
+	}
+
+	id, err := userModule.Register(user)
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	return server.WriteJSON(w, http.StatusCreated, schemas.RegisterUserResponse{Id: *id})
 }
 
 func CreateUser(s server.Server, w http.ResponseWriter, r *http.Request) error {
-	return nil
+	var request schemas.CreateUserRequest
+	if error := server.ReadJSON(r, &request.User); error != nil {
+		return server.SendError(w, error)
+	}
+
+	user := models.NewCreateUser(
+		request.User.Name,
+		request.User.Summary,
+		request.User.SemesterMatriculed,
+		request.User.GithubUsername,
+	)
+
+	userModule, err := s.Storage.GetUserModule()
+	if err != nil {
+		return err
+	}
+
+	id, err := userModule.Create(user)
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	return server.WriteJSON(w, http.StatusCreated, schemas.CreateUserResponse{Id: *id})
 }
 
 func UpdateUser(s server.Server, w http.ResponseWriter, r *http.Request) error {
-	return nil
+	idStr, err := server.GetRequestParam(r, "id")
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	id, err := strconv.ParseUint(*idStr, 10, 64)
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	var request schemas.UpdateUserRequest
+	if err := server.ReadJSON(r, &request); err != nil {
+		return server.SendError(w, err)
+	}
+
+	user := models.NewUser(
+		id,
+		request.User.Name,
+		request.User.Summary,
+		request.User.SemesterMatriculed,
+		request.User.GithubUsername,
+	)
+
+	userModule, err := s.Storage.GetUserModule()
+	if err != nil {
+		return err
+	}
+
+	if err := userModule.Update(user); err != nil {
+		return server.SendError(w, err)
+	}
+
+	return server.WriteJSON(w, http.StatusOK, nil)
 }
 
 func DeleteUser(s server.Server, w http.ResponseWriter, r *http.Request) error {
-	return nil
+	idStr, err := server.GetRequestParam(r, "id")
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	id, err := strconv.ParseUint(*idStr, 10, 64)
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	userModule, err := s.Storage.GetUserModule()
+	if err != nil {
+		return err
+	}
+
+	err = userModule.Delete(id)
+	if err != nil {
+		return server.SendError(w, err)
+	}
+
+	return server.WriteJSON(w, http.StatusOK, nil)
 }
