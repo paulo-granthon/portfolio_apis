dc73fc05364ca8a68c2003ea1ee9f98eb4d64364
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-03 17:51:16 -0300

    chore(back): move `/` requests to dedicated `endpoints` file

---
+++ api/endpoints/root.go
@@ -0,0 +1,35 @@
+package endpoints
+
+import (
+	"net/http"
+	"server"
+)
+
+func RootEndpoints() []server.Endpoint {
+	return []server.Endpoint{
+		{
+			Path: "/",
+			Methods: []server.Method{
+				server.NewMethod("GET", GetRoot),
+			},
+		},
+		{
+			Path: "/migrate",
+			Methods: []server.Method{
+				server.NewMethod("POST", Migrate),
+			},
+		},
+	}
+}
+
+func GetRoot(s server.Server, w http.ResponseWriter, r *http.Request) error {
+	return server.WriteJSON(w, http.StatusOK, "Hello, world!")
+}
+
+func Migrate(s server.Server, w http.ResponseWriter, r *http.Request) error {
+	err := s.Storage.Migrate()
+	if err != nil {
+		return server.SendError(w, err)
+	}
+	return server.WriteJSON(w, http.StatusOK, "Migrated")
+}
