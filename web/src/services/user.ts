b1b3bf3ac63e9321074addac07649d1b459bee8d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-04 19:57:50 -0300

    fix(front): explicit cast to `UserSchema` in `user` related `services`

--- web/src/services/user.ts
+++ web/src/services/user.ts
@@ -1,22 +1,43 @@
 import {
-    UserSchema,
-    RegisterUserSchema,
-    PostUserSchema,
-    UpdateUserSchema,
+  UserSchema,
+  RegisterUserSchema,
+  PostUserSchema,
+  UpdateUserSchema,
 } from "../schemas/user";
 
+import { YearSemester } from "../schemas/yearSemester";
+
 const API_URL = import.meta.env.VITE_API_URL;
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+export function mapUsers(data: any): UserSchema[] {
+  return data.map((user: any) => mapUser(user));
+}
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+export function mapUser(data: any): UserSchema {
+  return {
+    id: data.id,
+    name: data.name,
+    summary: data.summary,
+    semesterMatriculed: new YearSemester(
+      data.semesterMatriculed.year,
+      data.semesterMatriculed.semester,
+    ),
+    githubUsername: data.githubUsername,
+  };
+}
 
 export async function getUsers(): Promise<UserSchema[]> {
   return fetch(API_URL + "/users")
     .then((response) => response.json())
-    .then((data) => data);
+    .then((data) => mapUsers(data));
 }
 
 export async function getUser(id: number): Promise<UserSchema> {
   return fetch(API_URL + "/users/" + id)
     .then((response) => response.json())
-    .then((data) => data);
+    .then((data) => mapUser(data));
 }
 
 export async function registerUser(payload: RegisterUserSchema) {
