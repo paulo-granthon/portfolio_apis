13cb8ae12919bc0ef7e21db9c239098c652b0334
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-05 23:00:02 -0300

    feat(front): use `ProjectList` in `Portfolio` page

--- web/src/pages/portfolio.tsx
+++ web/src/pages/portfolio.tsx
@@ -1,20 +1,8 @@
-import Project from "../components/project";
 import User from "../components/user";
-import { ProjectSchema } from "../schemas/project";
+import ProjectList from "../components/projectList";
 import { UserSchema } from "../schemas/user";
 import * as styles from "../styles/portfolio";
 
-export default function Portfolio(user: UserSchema) {
-  const projects: ProjectSchema[] = [
-    {
-      id: 1,
-      name: "Project 1",
-      semester: 1,
-      summary: "This is a summary",
-      company: "Company 1",
-      url: "https://example.com",
-    },
-  ];
 interface PortfolioProps {
   user: UserSchema;
 }
@@ -26,14 +14,7 @@ export default function Portfolio({user}: PortfolioProps) {
         <h1>Portfolio</h1>
       </div>
       <User user={user} />
-      <div {...styles.projects}>
-        <h2 {...styles.projectsHeader}>Projects</h2>
-        <div>
-          {projects.map((project) => (
-            <Project key={project.name} project={project} />
-          ))}
-        </div>
-      </div>
+      <ProjectList user={user} />
     </div>
   );
 }
