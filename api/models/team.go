82c7ec41f95d83ef23db40db2eaab778c5dc94b5
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-05 21:29:09 -0300

    feat(back): create `Team` and `CreateTeam` models

---
+++ api/models/team.go
@@ -0,0 +1,28 @@
+package models
+
+type Team struct {
+	Id   uint64 `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
+}
+
+func NewTeam(
+	id uint64,
+	name string,
+) Team {
+	return Team{
+		Id:   id,
+		Name: name,
+	}
+}
+
+type CreateTeam struct {
+	Name string `json:"name" db:"name"`
+}
+
+func NewCreateTeam(
+	name string,
+) CreateTeam {
+	return CreateTeam{
+		Name: name,
+	}
+}
