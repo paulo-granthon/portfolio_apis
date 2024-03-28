83f88a3e92041a4d657ded208f9b03f9d34e8e03
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-27 21:37:58 -0300

    feat(back): create `NoteDetail` type in `models.note`

--- api/models/note.go
+++ api/models/note.go
@@ -24,6 +24,30 @@ func NewNote(
 	}
 }
 
+type NoteDetail struct {
+	Id      uint64 `json:"id" db:"id"`
+	Skill   string `json:"skill"`
+	Project string `json:"project"`
+	User    string `json:"user"`
+	Content string `json:"content" db:"content"`
+}
+
+func NewNoteDetail(
+	id uint64,
+	skill string,
+	project string,
+	user string,
+	content string,
+) NoteDetail {
+	return NoteDetail{
+		Id:      id,
+		Skill:   skill,
+		Project: project,
+		User:    user,
+		Content: content,
+	}
+}
+
 type CreateNote struct {
 	SkillId   uint64 `json:"skillId" db:"skillId"`
 	ProjectId uint64 `json:"projectId" db:"projectId"`
