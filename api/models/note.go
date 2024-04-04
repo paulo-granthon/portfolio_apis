de8a989779ce22de32ede1a8a9446d2c6f3c29eb
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-03 21:35:27 -0300

    refactor(back): standardize capitalization of `Id` related fields

--- api/models/note.go
+++ api/models/note.go
@@ -4,7 +4,7 @@ type Note struct {
 	Id        uint64 `json:"id" db:"id"`
 	SkillId   uint64 `json:"skillId" db:"skillId"`
 	ProjectId uint64 `json:"projectId" db:"projectId"`
-	UserID    uint64 `json:"userId" db:"userId"`
+	UserId    uint64 `json:"userId" db:"userId"`
 	Content   string `json:"content" db:"content"`
 }
 
@@ -12,14 +12,14 @@ func NewNote(
 	id uint64,
 	skillId uint64,
 	projectId uint64,
-	userID uint64,
+	userId uint64,
 	content string,
 ) Note {
 	return Note{
 		Id:        id,
 		SkillId:   skillId,
 		ProjectId: projectId,
-		UserID:    userID,
+		UserId:    userId,
 		Content:   content,
 	}
 }
@@ -51,20 +51,20 @@ func NewNoteDetail(
 type CreateNote struct {
 	SkillId   uint64 `json:"skillId" db:"skillId"`
 	ProjectId uint64 `json:"projectId" db:"projectId"`
-	UserID    uint64 `json:"userId" db:"userId"`
+	UserId    uint64 `json:"userId" db:"userId"`
 	Content   string `json:"content" db:"content"`
 }
 
 func NewCreateNote(
 	skillId uint64,
 	projectId uint64,
-	userID uint64,
+	userId uint64,
 	content string,
 ) CreateNote {
 	return CreateNote{
 		SkillId:   skillId,
 		ProjectId: projectId,
-		UserID:    userID,
+		UserId:    userId,
 		Content:   content,
 	}
 }
