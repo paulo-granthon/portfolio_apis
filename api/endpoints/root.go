2013ad883ffcb4c76522efecd7662882374c4213
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-04 22:35:57 -0300

    chore(back): remove `/migrate` endpoint

--- api/endpoints/root.go
+++ api/endpoints/root.go
@@ -13,12 +13,6 @@ func RootEndpoints() []server.Endpoint {
 				server.NewMethod("GET", GetRoot),
 			},
 		},
-		{
-			Path: "/migrate",
-			Methods: []server.Method{
-				server.NewMethod("POST", Migrate),
-			},
-		},
 	}
 }
 
@@ -26,20 +20,5 @@ func GetRoot(s server.Server, w http.ResponseWriter, r *http.Request) error {
 	return server.WriteJSON(
 		w, http.StatusOK,
 		"Hello, world!",
-	)
-}
-
-func Migrate(s server.Server, w http.ResponseWriter, r *http.Request) error {
-	err := s.Storage.Migrate()
-	if err != nil {
-		return server.SendError(
-			w, err, http.StatusInternalServerError,
-			"error migrating",
-		)
-	}
-
-	return server.WriteJSON(
-		w, http.StatusOK,
-		"Migrated",
 	)
 }
