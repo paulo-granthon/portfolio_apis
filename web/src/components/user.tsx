8e715f0184548df3d363dfc068f198254cbfc654
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-25 23:50:08 -0300

    chore(front): use `h2` for `user.name` in `User` component

--- web/src/components/user.tsx
+++ web/src/components/user.tsx
@@ -36,7 +36,7 @@ export default function User({ user }: UserProps) {
       </div>
       <div {...userStyles.cardRight}>
         <div {...userStyles.cardRightHeader}>
-          <h1>{user.name}</h1>
+          <h2>{user.name}</h2>
           <div {...userStyles.cardRightSemester}>
             <p>{userInitialSemester}</p>
             <p>Semestre Atual: {userCurrentSemester}</p>
